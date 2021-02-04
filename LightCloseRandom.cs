56604ae2cd91a57a3bfc3c57a4cabf5758951357
314588804@qq.com <314588804@qq.com>
2021-02-04 19:50:11 +0800

    添加项目文件。

---
+++ LightCloseRandom.cs
@@ -0,0 +1,34 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+
+
+namespace LightCloseRandom
+{
+    [PluginDetails(
+        author = "Mr.River",
+        name = "LightCloseRandom - 灯光关闭随机",
+        description = "随机概率触发设施内部进入持续性【停电】状态，让玩家体验到不一样的Site，并加强灯光类道具的使用频率",
+        version = "0.0.1"
+    )]
+
+    class LightCloseRandom : Plugin
+    {
+        public override void OnDisable()
+        {
+            this.Info("LightCloseRandom载入失败");
+        }
+
+        public override void OnEnable()
+        {
+            this.Info("LightCloseRandom载入成功");
+        }
+
+        public override void Register()
+        {
+            LightCloseRandomHandler eventhHander = new LightCloseRandomHandler(this);
+            //this.AddConfig("Set_Light_Enable_Percent", 0.5,  true, "");
+            this.AddEventHandlers(eventhHander, Priority.Normal);
+        }
+    }
+}
---
+++ LightCloseRandomHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Smod2;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.API;
+
+namespace LightCloseRandom
+{
+    class LightCloseRandomHandler : IEventHandlerRoundStart
+    {
+        public Plugin plugin;
+        
+        private Random random;
+        private bool enable;
+
+        private float setValue;
+        private List<Room> roomsList;
+        private Map map;
+
+        public LightCloseRandomHandler(Plugin plugin)
+        {         
+            this.plugin = plugin;
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            //获取配置信息
+            setValue = plugin.GetConfigFloat("Set_Light_Enable_Percent");
+            if (setValue == 0f) { setValue = 0.5f; }
+
+            roomsList = new List<Room>();
+            roomsList = ev.Server.Map.GetRooms();
+
+            map = ev.Server.Map;
+
+            //获取随机值作为启用插件的依据
+            float i = random.Next(0, 1);
+            //依据判断是否启用
+            if (i <= setValue) { enable = true; }
+            else { enable = false; }
+
+            if (enable == false) { return; }
+            else
+            {               
+                //map.OverchargeLights(900f, false);
+                //遍历整个房间，并关闭灯光
+                for (int n = 0; n < roomsList.Count; n++)
+                {
+                    roomsList[n].FlickerLights(20f);
+                }
+            }
+        }
+    }
+}
